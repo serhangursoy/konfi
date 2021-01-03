a349f00352ed9930d7df27fa533860b9e83c0063
Serhan Gürsoy <gursoyserhan@yandex.com>
2021-01-03 16:31:05 +0100

    Added simple error handling for better UI/UX. Added clickable links when you read the message. Added several UI tweaks for phones.

--- src/pages/ImageDecrypt.js
+++ src/pages/ImageDecrypt.js
@@ -20,21 +20,30 @@ export default class ImageDecrypt extends React.Component {
         var file = event.target.files[0];
         const canvas = this.canvasRef.current;
         var reader = new FileReader();
-        console.log(file)
         reader.onload = function (e) {
             const img = new Image();
             img.src = e.target.result;
             img.onload = () => {
-                canvas.width = img.width;
-                canvas.height = img.height;
-                const context = canvas.getContext('2d');
-                context.drawImage(img, 0, 0);
-                self.steganObj = new Steganography(context, img)
-                let hiddenText = self.steganObj.GetHiddenContent()
-                self.setState({ hiddenText: hiddenText, isUploaded: true });
+                try { 
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    const context = canvas.getContext('2d');
+                    context.drawImage(img, 0, 0);
+                    self.steganObj = new Steganography(context, img)
+                    let hiddenText = self.steganObj.GetHiddenContent()
+                    self.setState({ hiddenText: hiddenText, isUploaded: true });
+                }catch(e){
+                    self.setState({ haveError: true, isUploaded: true })
+                }
             }
         }
         reader.readAsDataURL(file);
+    }
+
+    replaceURLWithHTMLLinks(text) {
+      //eslint-disable-next-line
+      var exp = /(\b(https?|http|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+      return { __html: text.replace(exp,"<a target='_blank' href='$1'>$1</a>") } 
     }
 
     render() {
@@ -46,9 +55,10 @@ export default class ImageDecrypt extends React.Component {
                 <canvas className="ImageCanvas" ref={this.canvasRef} style={canvasStyle} />
                 <input id="fileUploadInput" type="file" accept="image/png" ref={(ref) => this.upload = ref} style={{ display: 'none' }}
                     onChange={this.onChangeFile.bind(this)} />
-                { this.state.isUploaded && <p style={{width: "100%"}}>Hidden text is: <div className="HiddenTextContainer">{this.state.hiddenText} </div> </p>}
+                { this.state.isUploaded && <div style={{width: "100%"}}>Hidden text is: <div className="HiddenTextContainer" dangerouslySetInnerHTML={this.replaceURLWithHTMLLinks(this.state.hiddenText)}></div> </div>}
                 <Button text="Upload" size="big" onClick={() => { this.upload.click() }}></Button>
                 <subtitle>This can take a while if your image is big. Please wait</subtitle>
+                { this.state.haveError && <div className="ErrorPanel">Oh! Something happened. It might be because your image is corrupted or we messed up something. Either way, you can't use this image.</div> }
             </div>
         )
     }
